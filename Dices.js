00e669be1e489464e906a1d8766edea558b9b645
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-02-25 14:48:17 +0200

    figured ranges

--- Dices.js
+++ Dices.js
@@ -1,4 +1,6 @@
 window.onload=function(){
+    var number="id"+(Math.floor(Math.random() * 6 ) +1);
+    console.log(number)
     var rand1=document.getElementById("d1")
     console.log(rand1.id)
     rand1.id="d2"
