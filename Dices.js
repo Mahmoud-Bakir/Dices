431624aa86c4d8f23d8903d1f6472928efd2e7eb
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-02-25 14:52:40 +0200

    added random ids

--- Dices.js
+++ Dices.js
@@ -1,9 +1,14 @@
 window.onload=function(){
-    var number="id"+(Math.floor(Math.random() * 6 ) +1);
-    console.log(number)
+    var number1="d"+(Math.floor(Math.random() * 6 ) +1);
+    console.log(number1)
+    var number2="d"+(Math.floor(Math.random() * 6 ) +1);
+    console.log(number2)
     var rand1=document.getElementById("d1")
+    rand1.id=number1
     console.log(rand1.id)
-    rand1.id="d2"
-    console.log(rand1.id)
+    var rand2=document.getElementById("d2")
+    rand2.id=number2
+    console.log(rand2.id)
+    
 
 }
